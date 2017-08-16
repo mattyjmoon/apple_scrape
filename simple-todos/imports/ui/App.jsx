8c3eef7dd5a230cd6354ce60c173b7582dcd49ea
Matthew Moon <mmoon@MatthewMoon-rMBP.reputation.lan>
2017-08-16 16:11:56 -0700

    Set up react

---
+++ simple-todos/imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import Task from './Task.jsx';
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <p>testing</p>
+      </div>
+    )
+  }
+}
